21d16ae1d873e7343321a6f00dac48ed8ac7b8ba
Junior <juniorn06@gmail.com>
2023-07-06 13:33:52 -0300

    Add exercício 1

---
+++ src/EstruturaSequencial/Ex_1.java
@@ -0,0 +1,11 @@
+package EstruturaSequencial;
+
+public class Ex_1 {
+
+    //Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+
+    public static void main(String[] args) {
+
+        System.out.println("Alô mundo!");
+    }
+}
